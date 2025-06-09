1fce3e847679690ce3508968b67b5f70e78c650d
Johanna Räisä <johanna.raisa@gmail.com>
2025-06-09 10:32:39 +0300

    Add try catch to skip errors

--- Pate/misc/set_suomifi_messaging.pl
+++ Pate/misc/set_suomifi_messaging.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use C4::Context;
+use Try::Tiny;
 
 # This script is used to set up the SuomiFi messaging system in Koha.
 # It inserts a new message transport type 'suomifi' into the database
@@ -20,11 +21,12 @@ my $message_transports = $dbh->selectall_arrayref("SELECT message_attribute_id, 
 
 foreach my $row (@$message_transports) {
     my ($message_attribute_id, $message_transport_type, $is_digest, $letter_module, $letter_code) = @$row;
-    if ($message_transport_type eq 'suomifi') {
-        next; # Skip if already 'suomifi'
-    }
-    print "Inserting: $message_attribute_id, $message_transport_type, $is_digest, $letter_module, $letter_code\n";
+    print "Inserting: $message_attribute_id, suomifi, $is_digest, $letter_module, $letter_code\n";
     my $sth = $dbh->prepare("INSERT INTO message_transports (message_attribute_id, message_transport_type, is_digest, letter_module, letter_code) VALUES (?, ?, ?, ?, ?)");
-    $sth->execute($message_attribute_id, 'suomifi', $is_digest, $letter_module, $letter_code) or die $dbh->errstr;
+    try {
+        $sth->execute($message_attribute_id, 'suomifi', $is_digest, $letter_module, $letter_code);
+    } catch {
+        warn "Failed to insert message transport: $_";
+    };
     $sth->finish();
 }
